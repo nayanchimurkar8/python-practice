10ba97d9a345e78cb162175f662688bc026130b4
Nayan Chimurkar <nayanchimurkar8@gmail.com>
2026-09-15 09:57:36 +0530

    py

---
+++ new2.py
@@ -0,0 +1,8 @@
+correct_pass = "some_pass"
+not_found = True
+while not_found:
+    passw = input("Enter the pass:")
+    if passw == correct_pass:
+        not_found = False
+    print("password matched!")
+    
